96b6d22d94d1123de5863761d7273ded3158641e
jagsher2002 <157498483+jagsher2002@users.noreply.github.com>
2024-10-07 17:33:55 -0400

    update

---
+++ ThreadHaven/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ThreadHaven.Models;
+
+namespace ThreadHaven.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+       // add 1 DbSet object for each model which can perform CRUD operations
+       public DbSet<Product> Products { get; set; }
+       public DbSet<Category> Categories { get; set; }
+       public DbSet<CartItem> CartItems { get; set; }
+       public DbSet<OrderDetail> OrderDetails { get; set; }    
+       public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ ThreadHaven/Data/Migrations/20240924134358_AddPropertiesToProduct.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThreadHaven.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Products");
+        }
+    }
+}
